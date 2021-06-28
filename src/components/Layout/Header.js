b34ebc531f4f31924bf9aed502b4c84e014fdab8
Simone Di Fonzo <simonedf@nandos-ltmb-072.local>
2021-06-28 15:22:28 +0100

    Header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Style from 
+
+const Header = (props) => {
+  return <React.Fragment>
+    <Header>
+      <h1>ReactMeal</h1>
+      <button>Cart</button>
+    </Header>
+    <div>
+      <img />
+    </div>
+  </React.Fragment>  
+};
+
+export default Header;
