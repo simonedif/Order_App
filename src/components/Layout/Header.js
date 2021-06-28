4e9e1d613d2bc533265c190ec2bf0e6ef34f29ec
Simone Di Fonzo <simonedf@nandos-ltmb-072.local>
2021-06-28 17:28:03 +0100

    Header & Meals Structure

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import Style from 
+
+import mealsImage from '../../assets/meal.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
-  return <React.Fragment>
-    <Header>
-      <h1>ReactMeal</h1>
-      <button>Cart</button>
-    </Header>
-    <div>
-      <img />
-    </div>
-  </React.Fragment>  
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Testing WebPage</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Header;
