f25f01d14f3440fda3608d92c27f3de564d1787b
simonedf <simonedifonzo91@gmail.com>
2021-08-24 00:14:34 +0100

    Loader + Error Handler

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,52 +7,78 @@ import style from "./AvaiableMeal.module.css";
 import Card from "../UI/Card";
 import MealItem from "../Meals/MealItem/MealItem";
 //import DATA from './MealItem/avaliable_products';
-import ProductType from '../Layout/ProductType';
-
+import ProductType from "../Layout/ProductType";
+//import LoaderError from '../UI/LoaderError';
+import StyleLoader from "../UI/loader.module.css";
 
 //Passing Data to The MealComponent
 const AvaiableMeal = (props) => {
-
   //Fetch Function
   const [fetchData, setFetchData] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-    
     const fetchMeals = async () => {
+      const response = await fetch(
+        "https://order-app-2cddf-default-rtdb.europe-west1.firebasedatabase.app/menu.json"
+      );
 
-      const response = await fetch('https://order-app-2cddf-default-rtdb.europe-west1.firebasedatabase.app/menu.json');
-      const responseData = await response.json()
+      if (!response.ok) {
+        throw new Error('Error');
+      }
+
+      const responseData = await response.json();
+
       setFetchData(responseData);
-    }
-    
-    fetchMeals();
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
-
-//Button have all the Items - Remove Filter button
-  const allProducts = ['All', ...new Set(fetchData.map(item => item.type))];
-  console.log(allProducts);
-
-
-  const [menu, setMenu] = useState(fetchData)
-  const [activeButton, setActiveButton] = useState('All')
 
   useEffect(() => {
     setMenu(fetchData);
   }, [fetchData]);
 
-  const filter = (button) => {
+  //Button have all the Items - Remove Filter button
+  const allProducts = ["All", ...new Set(fetchData.map((item) => item.type))];
+  console.log(allProducts);
 
+  const [menu, setMenu] = useState(fetchData);
+  const [activeButton, setActiveButton] = useState("All");
+
+  const filter = (button) => {
+    
     //Set Button All
-    if (button === 'All') {
+    if (button === "All") {
       setMenu(fetchData);
-      setActiveButton('All');
-      return
-    };
-    const filterData = fetchData.filter(item => item.type === button);
-    setMenu(filterData)
-    setActiveButton(button)
+      setActiveButton("All");
+      return;
+    }
+    const filterData = fetchData.filter((item) => item.type === button);
+    setMenu(filterData);
+    setActiveButton(button);
+  };
+
+  if (isloading) {
+    return (
+      <section className={StyleLoader.loader}>
+      </section>
+    );
   }
-  
+
+  if (httpError) {
+    return (
+      <div className={StyleLoader.loadingText}>
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
   const mealsList = menu.map((meal) => (
     <MealItem
       id={meal.id}
@@ -66,7 +92,11 @@ const AvaiableMeal = (props) => {
   return (
     <section className={style.meals}>
       <Card>
-        <ProductType activeButton={activeButton} button={allProducts} filter={filter}  />
+        <ProductType
+          activeButton={activeButton}
+          button={allProducts}
+          filter={filter}
+        />
         <ul>{mealsList}</ul>
       </Card>
     </section>
---
+++ src/components/UI/LoaderError.js
@@ -0,0 +1,10 @@
+//Component not in use
+
+import React from 'react';
+import Style from './loader.module.css'
+
+const LoaderError = (props) => {
+  <div className={Style.loader}>Loading...</div>
+};
+
+export default LoaderError;
