38d079ced545521206b05ca8014434dd4ef73896
simonedf@nandos.co.uk <simonedf@nandos.co.uk>
2022-06-08 23:01:09 +0100

    fix link firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,7 +9,6 @@ import StyleLoader from "../UI/loader.module.css";
 
 //Passing Data to The MealComponent
 const AvaiableMeal = (props) => {
-  
   //State Dealing With Fetch
   const [fetchData, setFetchData] = useState([]);
   const [isloading, setIsLoading] = useState(true);
@@ -18,7 +17,7 @@ const AvaiableMeal = (props) => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://console.firebase.google.com/project/order-app-2cddf/database/order-app-2cddf-default-rtdb/data/~2F/menu"
+        "https://order-app-2cddf-default-rtdb.europe-west1.firebasedatabase.app/menu.json"
       );
       console.log(response);
 
